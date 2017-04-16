3c2ad5d427aed3de6bfb6f68e819e8f0f9221ec7
vainotuisk <vainotuisk@gmail.com>
2017-04-16 22:24:20 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 $.ajaxSetup({ cache: false });
-   $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+   $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
 // $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>");
    $(".quote-content").html(a[0].content);
      // Only change code above this line.
@@ -14,5 +14,6 @@ $.ajaxSetup({ cache: false });
      // Only change code above this line.
      $(".quote-source").html(a[0].title);
    });
+ cache: false
  });
 });
