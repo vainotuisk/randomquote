4663482d7219979bdd4ad7cd8d0b8e0d47d8ffc8
vainotuisk <vainotuisk@gmail.com>
2017-04-17 22:25:16 +0300

    twitteriga

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
+  var quote;
+  var author;
 $.ajaxSetup({ cache: false });
-   $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-// $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>");
-   $(".quote-content").html(a[0].content);
-     // Only change code above this line.
-     $(".quote-source").html(a[0].title);
+   $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+   quote = a[0].content.replace(/<\/?[^>]+>/gi, '');
+    author= a[0].title.replace(/<\/?[^>]+>/gi, '');
+     console.log('tsitaat on: '+quote);
+     console.log('autor on: '+author);
+     $(".quote-content").html(quote);
+     $(".quote-source").html(author);
    });
    $('#getQuote').on('click', function(e) {
    e.preventDefault();
    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
- // $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>");
-   $(".quote-content").html(a[0].content);
-     // Only change code above this line.
-     $(".quote-source").html(a[0].title);
+     quote = a[0].content.replace(/<\/?[^>]+>/gi, '');
+    author= a[0].title.replace(/<\/?[^>]+>/gi, '');
+   $(".quote-content").html(quote);
+     $(".quote-source").html(author);
    });
+
+ });
+  $('#shareTwitter').on('click', function(e) {
+   e.preventDefault();
+   console.log("Nupuvajutuds");
+    window.open('https://twitter.com/intent/tweet?text='+quote +' - ('+ author+')');
+
  });
 });
